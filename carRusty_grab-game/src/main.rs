1c79049974020125b2c06a08e9fa0125ca533f7d
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-08-12 21:10:29 +1200

    colliders, sprites and movement: completed

--- carRusty_grab-game/src/main.rs
+++ carRusty_grab-game/src/main.rs
@@ -26,18 +26,53 @@ fn main() {
     let player = game.add_sprite("player", SpritePreset::RacingCarRed);
     player.translation = Vec2::new(0.00, 0.00);
     player.rotation = std::f32::consts::FRAC_PI_2;
-    player.scale = 0.5;
-    player.layer = 1.0;
+    player.collision = true;
 
-    let temporary = game.add_sprite("temporary", SpritePreset::RacingCarBlue);
-    temporary.translation = Vec2::new(30.00, 0.00);
-    temporary.layer = 1.1;
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.00, 0.00);
+    car1.collision = true;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-//    game_state.current_score += 1;
-//    println!("CURRENT SCORE: {}", game_state.current_score);
+    // handle collisions
+for event in engine.collision_events.drain(..) {
+    if event.state == CollisionState::Begin && event.pair.one_starts_with("player") {
+        // remove the enemy car
+        for label in [event.pair.0, event.pair.1] {
+            if label != "player" {
+                engine.sprites.remove(&label);
+            }
+        }
+        game_state.current_score += 1;
+        println!("CURRENT SCORE: {}", game_state.current_score);
+    }
+}
+
+// handle movements
+let player = engine.sprites.get_mut("player").unwrap();
+const MOVEMENT_SPEED: f32 = 100.00;
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Up, KeyCode::W]) {
+        player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+
+    }
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Down, KeyCode::S]) {
+        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+
+    }
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Right, KeyCode::D]) {
+        player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
+
+    }
+
+    if engine.keyboard_state.pressed_any(&[KeyCode::Left, KeyCode::A]) {
+        player.translation.x -= MOVEMENT_SPEED * engine.delta_f32;
+
+    }
+
 }
