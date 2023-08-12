9f79832f025ec9b47cdc7da4ca942e5a43f13e4c
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-08-12 16:53:56 +1200

    player layers and configd

--- carRusty_grab-game/src/main.rs
+++ carRusty_grab-game/src/main.rs
@@ -23,12 +23,21 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
+    let player = game.add_sprite("player", SpritePreset::RacingCarRed);
+    player.translation = Vec2::new(0.00, 0.00);
+    player.rotation = std::f32::consts::FRAC_PI_2;
+    player.scale = 0.5;
+    player.layer = 1.0;
+
+    let temporary = game.add_sprite("temporary", SpritePreset::RacingCarBlue);
+    temporary.translation = Vec2::new(30.00, 0.00);
+    temporary.layer = 1.1;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-   game_state.current_score += 1;
-   println!("CURRENT SCORE: {}", game_state.current_score);
+//    game_state.current_score += 1;
+//    println!("CURRENT SCORE: {}", game_state.current_score);
 }
